ef05eeec02593870e7ddd806dfe74292c9a37077
lilansheng <945004642@qq.com>
2024-02-26 21:13:21 +0800

    实现评论删除

--- src/App.js
+++ src/App.js
@@ -76,6 +76,10 @@ const tabs = [
 
 const App = () => {
   const [commentList, setCommentList] = useState(defaultList)
+  const handleDel = rpid => {
+    console.log('rpid: ', rpid)
+    setCommentList(commentList.filter(item => item.rpid !== rpid))
+  }
   return (
     <div className="app">
       {/* 导航 Tab */}
@@ -120,24 +124,28 @@ const App = () => {
               {/* 头像 */}
               <div className="root-reply-avatar">
                 <div className="bili-avatar">
-                  <img className="bili-avatar-img" alt="" src={ item.user.avatar} />
+                  <img className="bili-avatar-img" alt="" src={item.user.avatar} />
                 </div>
               </div>
 
               <div className="content-wrap">
                 {/* 用户名 */}
                 <div className="user-info">
-                  <div className="user-name">{ item.user.uname}</div>
+                  <div className="user-name">{item.user.uname}</div>
                 </div>
                 {/* 评论内容 */}
                 <div className="root-reply">
-                  <span className="reply-content">{ item.content}</span>
+                  <span className="reply-content">{item.content}</span>
                   <div className="reply-info">
                     {/* 评论时间 */}
                     <span className="reply-time">{item.ctime}</span>
                     {/* 评论数量 */}
                     <span className="reply-time">点赞数:{item.like}</span>
-                    <span className="delete-btn">删除</span>
+                    {user.uid === item.user.uid && (
+                      <span className="delete-btn" onClick={() => handleDel(item.rpid)}>
+                        删除
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
